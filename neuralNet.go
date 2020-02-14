9519633fbee9a6f83d2f2e589d1ec6ab561cbb75
Mirza Belal Ahmed <mirza360@live.com>
2020-02-15 00:33:12 +0600

    loss function from backpropagation

--- neuralNet.go
+++ neuralNet.go
@@ -2,62 +2,63 @@ package main
 
 /*
 type node struct {
-	variable float32
-	weight   float32
-	bias     float32
+	variable float64
+	weight   float64
+	bias     float64
 	next     *node
 }
 */
 func main() {
 
-	var a float32 = 1
-	var b float32 = 1
-	var x float32 = 2
-	var y float32 = 3
-	train(a, b, x, y)
+	var a float64 = 1
+	var b float64 = 1
+	var x float64 = 3
+	var y float64 = 5
+
+	//var MSE float64
+
+	var lossA, lossB float64 = train(a, b, x, y)
+	println("Here is the loss")
+	println(lossA)
+	println(lossB)
 
 }
-func train(a, b, x, y float32) {
+func train(a, b, x, y float64) (float64, float64) {
 	//forward pass initiation
-	var i [6]float32
+	var i [6]float64
 	i[1] = a * x
 	i[2] = i[1] + b
 	i[3] = i[2]
 	i[4] = y - i[3]
 	i[5] = i[4] * i[4]
+
 	//backpropagation
 	//differential stuff
-	var diff [5]float32
+	var diff [5]float64
 	//di5/di4
 	diff[4] = 2 * i[4]
 	//di4/di3
 	diff[3] = -1
 	//di3/di2
 	diff[2] = 1
-	//di2/di1
+	//di2/di1 	useful only for di5/da
 	diff[1] = b
 	//overlap cost
 	var c int
-	var overlapCost float32 = 1
-	for c = 1; c < 5; c++ {
+	var overlapCost float64 = 1
+	for c = 2; c < 5; c++ {
 		overlapCost = overlapCost * diff[c]
 	}
-	//di1/db=x
-	//var costA float32 = overlapCost * x
+	//di1/db=x diff[1] has di2/di1
+	var costA float64 = overlapCost * diff[1] * x
 
 	//di1/db=i1
-	//var costB float32 = overlapCost * i[1]
-	for c = 1; c < 6; c++ {
-		println(i[c])
+	var costB float64 = overlapCost * i[1]
 
-	}
-	println()
-	for c = 1; c < 5; c++ {
-		println(diff[c])
+	//end of differentiation stuff
 
-	}
-
-	//println(costA)
-	//println(costB)
+	//loss function
+	//var loss float64 = i[5] + math.Abs(a) + math.Abs(b)
+	return costA, costB
 
 }
