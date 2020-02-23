2d2c7875bd5eef25ec6229c22e4cdb04f69a0a19
Shazzad Sakim Sourav <shazzad.sourav@northsouth.edu>
2020-02-23 21:14:55 +0600

    Made Multilevel linked list added

--- scripts/linkedList/linkedList.go
+++ scripts/linkedList/linkedList.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 type node struct {
-	num  int
-	prev *node
-	next *node
+	idx       int
+	varA      float64
+	varB      float64
+	resVal    float64
+	prev      *node
+	next      *node
+	childHead *bottomNode
+	childTail *bottomNode
 }
 
 type list struct {
-	tail  *node
-	start *node
+	tail *node
+	head *node
+}
+
+type bottomNode struct {
+	diffVal  float64
+	upNode   *bottomNode
+	downNode *bottomNode
 }
 
 func (l *list) Display() {
-	list := l.start
+	list := l.head
 	for list != nil {
 		fmt.Printf("value = %v and prev = %v and next= %v\n", list.num, list.prev, list.next)
 		list = list.next
@@ -28,15 +38,15 @@ func (l *list) Display() {
 func (l *list) DisplayReverse() {
 	list := l.tail
 	for list != nil {
-		fmt.Printf("value = %v\n", list.num)
+		fmt.Printf("value = %v\n", list.idx)
 		list = list.prev
 	}
 	fmt.Println()
 }
 
 func (l *list) insertNode(newNode *node) {
-	if l.start == nil {
-		l.start = newNode
+	if l.head == nil {
+		l.head = newNode
 		l.tail = newNode
 	} else {
 		currentNode := l.start
@@ -49,8 +59,25 @@ func (l *list) insertNode(newNode *node) {
 	}
 }
 
+func (n *node) addBottomNode(newNode *bottomNode) {
+	if n.prev == nil {
+		println("No parent Node")
+	} else if n.childHead == nil {
+		n.childHead = newNode
+		n.childTail = newNode
+	} else {
+		currentNode := n.childHead
+		for currentNode.downNode != nil {
+			currentNode = currentNode.downNode
+		}
+		newNode.upNode = currentNode
+		currentNode.downNode = newNode
+		n.childTail = newNode
+	}
+}
+
 func main() {
-	items := &list{}
+	/*items := &list{}
 	size := 10
 	//rand_number := make([]int, size, size)
 	for i := 0; i < size; i++ {
@@ -61,7 +88,20 @@ func main() {
 
 		}
 		items.insertNode(&node)
+
 		fmt.Printf("%v and number is %v\n", i, node.num)
 	}
 	items.Display()
+	items := &list{}
+
+	node1 := node{num: 1}
+	items.insertNode(&node1)
+	node2 := node{num: 2}
+	items.insertNode(&node2)
+	node3 := node{num: 3}
+	items.addBottomNode(&node3)
+	node4 := node{num: 4}
+	items.addBottomNode(&node4)
+
+	items.Display()*/
 }
