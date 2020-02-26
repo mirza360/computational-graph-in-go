60a50e41d1d4adcc5add337b1713c889aba16b3c
Shazzad Sakim Sourav <shazzad.sourav@northsouth.edu>
2020-02-27 00:15:27 +0600

    A simle traversal added

--- scripts/sample.go
+++ scripts/sample.go
@@ -14,17 +14,14 @@ var y float64 = 2.2
 var learnigRate float64 = 0.005
 
 type node struct {
-	idx       int
-	varA      float64
-	varB      float64
-	varTemp   float64
-	resVal    float64
-	diffVal   float64
-	str       string
-	prev      *node
-	next      *node
-	childDown *node
-	childUp   *node
+	resVal   float64
+	diffVal1 float64
+	diffVal2 float64
+	str      string
+	flg      string
+	calcDiff float64
+	prev     *node
+	next     *node
 }
 
 type list struct {
@@ -33,45 +30,10 @@ type list struct {
 }
 
 func main() {
-	//solving for y=(c(ax+b)^2+d)
-	/*
-		var a float64 = 1.5
-		var b float64 = 1.7
-		var c float64 = 1
-		var d float64 = 1
-		var learnigRate float64 = 0.5
-		link := node{}
-		head := root{}
-	*/
-	//head.head:=link
-	//head.tail:=link
-
-	var data [3][2]float64
-	data[0][0] = float64(2)
-	data[0][1] = float64(17)
-	data[1][0] = float64(3)
-	data[1][1] = float64(37)
-	data[2][0] = float64(4)
-	data[2][1] = float64(65)
-	//println(gradB)
-
-	//var MSE float64
-
-	var a float64
-	var x float64
-	a = 2
-	x = 3
-	//var m float64 = 0
-	//var di float64 = 0
-	m, di, st := sqr(x, a, x, "x")
-	println(m)
-	println(di)
-	println(st)
 
 	item := nodeOps()
-
-	item.Display2D()
 	item.Display()
+	//item.DisplayReverse()
 
 }
 
@@ -90,58 +52,23 @@ func (l *list) addNode(newNode *node) {
 	}
 }
 
-/*func (l *list) addBottomNode(newNode *node) {
-	currentNode := l.head
-	if currentNode.childDown == nil {
-		currentNode.childDown = newNode
-		currentNode.childUp = newNode
-	} else {
-		currentNode := l.head
-		for currentNode.childDown != nil {
-			currentNode = currentNode.childDown
-		}
-		newNode.childUp = currentNode
-		currentNode.childDown = newNode
-		l.tail = newNode
-	}
-}*/
-
-func (n *node) addBottomNode(newNode *node) {
-	if n.childDown == nil {
-		n.childDown = newNode
-		n.childUp = newNode
-	} else {
-		currentNode := n.childDown
-		for currentNode.childDown != nil {
-			currentNode = currentNode.childDown
-		}
-		newNode.childUp = currentNode
-		currentNode.childDown = newNode
-		n.childUp = newNode
-	}
-}
-
-func (l *list) Display2D() {
+func (l *list) Display() {
 	list := l.head
 	//listBottom := list.childDown
 	for list != nil {
-		fmt.Printf("idx = %v and resVal = %v and str = %#v\n", list.idx, list.resVal, list.str)
-		for list.childDown != nil {
-			fmt.Printf("diffVal = %v and str = %#v\n", list.diffVal, list.str)
-			fmt.Println("check")
-			list = list.childDown
-		}
+		fmt.Printf("resVal = %v & diffVal1 = %v & diffVal2 = %v & str = %#v & flag = %#v & calcDiff = %v\n", list.resVal, list.diffVal1, list.diffVal2, list.str, list.flg, list.calcDiff)
 		list = list.next
 	}
 	fmt.Println()
 }
 
-func (l *list) Display() {
-	list := l.head
-	//listBottom := list.childDown
+func (l *list) DisplayReverse() {
+	list := l.tail
 	for list != nil {
-		fmt.Printf("idx = %v and resVal = %v and str = %#v\n", list.idx, list.resVal, list.str)
-		list = list.next
+		if list.flg == "" {
+			fmt.Printf("resVal = %v\n", list.resVal)
+			list = list.prev
+		}
 	}
 	fmt.Println()
 }
@@ -149,65 +76,52 @@ func (l *list) Display() {
 func nodeOps() *list {
 	//solving for y=(c(ax+b)^2+d)^2
 	items := &list{}
-	node0 := node{idx: 0, resVal: x}
-	items.addNode(&node0)
-
-	i1, df, s := mult(a, node0.resVal, node0.resVal, "x")
-	r1, df1, s1 := mult(a, node0.resVal, a, "a")
-	node1 := node{idx: 1, resVal: i1, varTemp: r1}
-	node1bottomNode1 := node{diffVal: df, str: s}
-	node1bottomNode2 := node{diffVal: df1, str: s1}
+	i1, df1 := sqr(x, 2, x)
+	node1 := node{resVal: i1, diffVal1: df1}
 	items.addNode(&node1)
-	node1.addBottomNode(&node1bottomNode1)
-	node1.addBottomNode(&node1bottomNode2)
 
-	i2, df2, s2 := add(node1.resVal, b, b, "b")
-	r2, df3, s3 := add(node1.resVal, b, node1.resVal, "i1")
-	node2 := node{idx: 2, resVal: i2, varTemp: r2}
-	node2bottomNode1 := node{diffVal: df2, str: s2}
-	node2bottomNode2 := node{diffVal: df3, str: s3}
+	i2, df2 := mult(c, i1, i1)
+	i2, df3 := mult(c, i1, c)
+	node2 := node{resVal: i2, diffVal1: df2, diffVal2: df3, str: "c"}
 	items.addNode(&node2)
-	node2.addBottomNode(&node2bottomNode1)
-	node2.addBottomNode(&node2bottomNode2)
 
-	i3, df4, s4 := sqr(node2.resVal, 2, i2, "i2")
-	node3 := node{idx: 3, resVal: i3, diffVal: df4, str: s4}
+	i3, df4 := mult(b, x, x)
+	i3, df5 := mult(b, x, b)
+	node3 := node{resVal: i3, diffVal1: df4, diffVal2: df5, str: "b", flg: "flag"}
 	items.addNode(&node3)
 
-	i4, df5, s5 := mult(c, node3.resVal, node3.resVal, "i3")
-	r3, df6, s6 := mult(c, node3.resVal, c, "c")
-	node4 := node{idx: 4, resVal: i4, varTemp: r3}
-	node4bottomNode1 := node{diffVal: df5, str: s5}
-	node4bottomNode2 := node{diffVal: df6, str: s6}
+	i4, df6 := add(i3, a, i3)
+	i4, df7 := add(i3, a, a)
+	node4 := node{resVal: i4, diffVal1: df6, diffVal2: df7, str: "a", flg: "flag"}
 	items.addNode(&node4)
-	node4.addBottomNode(&node4bottomNode1)
-	node4.addBottomNode(&node4bottomNode2)
 
-	i5, df7, s7 := add(node4.resVal, d, d, "d")
-	r4, df8, s8 := add(node4.resVal, d, node4.resVal, "i4")
-	node5 := node{idx: 5, resVal: i5, varTemp: r4}
-	node5bottomNode1 := node{diffVal: df7, str: s7}
-	node5bottomNode2 := node{diffVal: df8, str: s8}
+	i5, df8 := sqr(i4, 2, 14)
+	node5 := node{resVal: i5, diffVal1: df8, flg: "flag"}
 	items.addNode(&node5)
-	node5.addBottomNode(&node5bottomNode1)
-	node5.addBottomNode(&node5bottomNode2)
 
-	i6, df9, s9 := sqr(node5.resVal, 2, d, "d")
-	node6 := node{idx: 6, resVal: i6, diffVal: df9, str: s9}
+	i6, df9 := add(i2, i5, i2)
+	i6, df10 := add(i2, i5, i5)
+	node6 := node{resVal: i6, diffVal1: df9, diffVal2: df10}
 	items.addNode(&node6)
 
-	i7, df10, s10 := add(node6.resVal, y, i6, "i6")
-	node7 := node{idx: 7, resVal: i7, diffVal: df10, str: s10}
+	i7, df11 := add(y, -i6, i6)
+	node7 := node{resVal: i7, diffVal1: df11}
 	items.addNode(&node7)
 
-	i8, df11, s11 := sqr(node7.resVal, 2, i7, "i7")
-	node8 := node{idx: 8, resVal: i8, diffVal: df11, str: s11}
+	i8, df12 := sqr(i7, 2, i7)
+	node8 := node{resVal: i8, diffVal1: df12, calcDiff: 1}
 	items.addNode(&node8)
+
+	currentNode := items.tail
+	for currentNode.prev != nil {
+		currentNode.prev.calcDiff = currentNode.calcDiff + currentNode.diffVal1
+		currentNode = currentNode.prev
+	}
 
 	return items
 }
 
-func mult(a, b, d float64, str string) (float64, float64, string) {
+func mult(a, b, d float64) (float64, float64) {
 	var multAB float64 = a * b
 	//diff
 	var diffAB float64
@@ -216,11 +130,11 @@ func mult(a, b, d float64, str string) (float64, float64, string) {
 	} else if b == d {
 		diffAB = a
 	}
-	str1 := str
-	return multAB, diffAB, str1
 
+	return multAB, diffAB
 }
-func add(a, b, d float64, str string) (float64, float64, string) {
+
+func add(a, b, d float64) (float64, float64) {
 	var addAB float64 = a + b
 	//diff
 	var diffAB float64
@@ -229,11 +143,11 @@ func add(a, b, d float64, str string) (float64, float64, string) {
 	} else if b == d {
 		diffAB = b / math.Abs(b)
 	}
-	str1 := str
-	return addAB, diffAB, str1
 
+	return addAB, diffAB
 }
-func sqr(a, b, d float64, str string) (float64, float64, string) {
+
+func sqr(a, b, d float64) (float64, float64) {
 	var sqrAB float64 = a
 	var c int
 	for c = 1; c < int(b); c++ {
@@ -245,7 +159,5 @@ func sqr(a, b, d float64, str string) (float64, float64, string) {
 	//diff
 	var diffAB float64 = b * (sqrAB / a)
 
-	str1 := str
-
-	return sqrAB, diffAB, str1
+	return sqrAB, diffAB
 }
